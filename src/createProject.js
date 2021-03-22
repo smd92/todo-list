25dbaf7cdf7cfdaeaf5a739b80a497c9b0ee0c90
Sassan Marouf-Darvish <sejjil2@Sassans-Air.local>
2021-03-22 22:48:42 +0100

    added project methods

--- src/createProject.js
+++ src/createProject.js
@@ -6,6 +6,16 @@ class Project {
         this.dueDate = projectData.dueDate;
         this.priority = projectData.priority;
         this.notes = projectData.notes;
+        this.todoList = [];
+    }
+
+    addTodo(todo) {
+        this.todoList.push(todo);
+    }
+
+    removeTodo(todo) {
+        let index = this.todoList.indexOf(todo);
+        this.todoList.splice(index, 1);
     }
 }
 
