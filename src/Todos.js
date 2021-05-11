0ee512a8c437fb286e6026287dfe4e8e5f90eea9
sassan <sassan.marouf@gmail.com>
2021-05-11 19:25:16 +0200

    prettier

--- src/Todos.js
+++ src/Todos.js
@@ -1,74 +1,74 @@
 //create todo lists
 class TodoList {
-    constructor(projectData) {
-        this.visibleName = projectData[0];
-        this.nameDOM = this.visibleName.split(" ").join("-") + "List";
-        this.items = [];
-    }
-    
-    addItem(item) {
-        this.items.push(item);
-    }
+  constructor(projectData) {
+    this.visibleName = projectData[0];
+    this.nameDOM = this.visibleName.split(" ").join("-") + "List";
+    this.items = [];
+  }
 
-    removeItem(item) {
-        this.items.splice(item.index, 1);
-    }
+  addItem(item) {
+    this.items.push(item);
+  }
 
-    enumerateItems() {
-        for (let i = 0; i < this.items.length; i++) {
-            this.items[i].index = i;
-        }
-    }
+  removeItem(item) {
+    this.items.splice(item.index, 1);
+  }
 
-    setItemsListName() {
-        this.items.forEach((item) => {
-            item.listName = this.name;
-        })
+  enumerateItems() {
+    for (let i = 0; i < this.items.length; i++) {
+      this.items[i].index = i;
     }
+  }
+
+  setItemsListName() {
+    this.items.forEach((item) => {
+      item.listName = this.name;
+    });
+  }
 }
 
 //create todo items
 class Todo {
-    constructor(todoData) {
-        this.title = todoData.title;
-        this.description = todoData.description;
-        this.dueDate = todoData.dueDate;
-        this.priority = todoData.priority;
-        this.index;
-        this.listName;
-    }
+  constructor(todoData) {
+    this.title = todoData.title;
+    this.description = todoData.description;
+    this.dueDate = todoData.dueDate;
+    this.priority = todoData.priority;
+    this.index;
+    this.listName;
+  }
 }
 
 const todoListManager = (function () {
-    let allTodoLists = [];
+  let allTodoLists = [];
 
-    function addTodoList(todoList) {
-        allTodoLists.push(todoList);
-    }
+  function addTodoList(todoList) {
+    allTodoLists.push(todoList);
+  }
 
-    function pushTodoInCorrectList(listName, item) {
-        allTodoLists.forEach((list) => {
-            if (list.nameDOM === listName) {
-                list.items.push(item);
-            }
-        })
-    }
+  function pushTodoInCorrectList(listName, item) {
+    allTodoLists.forEach((list) => {
+      if (list.nameDOM === listName) {
+        list.items.push(item);
+      }
+    });
+  }
 
-    function getTodoLists() {
-        return allTodoLists;
-    }
+  function getTodoLists() {
+    return allTodoLists;
+  }
 
-    //TEST
-    function printLists() {
-        console.log(allTodoLists);
-    }
+  //TEST
+  function printLists() {
+    console.log(allTodoLists);
+  }
 
-    return {
-        addTodoList,
-        pushTodoInCorrectList,
-        getTodoLists,
-        printLists
-    }
+  return {
+    addTodoList,
+    pushTodoInCorrectList,
+    getTodoLists,
+    printLists,
+  };
 })();
 
 export { Todo, TodoList, todoListManager };
